6cfd25257fdf7deedfafc604dba8140e40e10791
Tatu Alatalo <n4927@student.jamk.fi>
2019-09-29 11:00:11 +0300

    Added Visual Studio Solution

---
+++ Code/Demo03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
