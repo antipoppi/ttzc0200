c9f3100d3af72b661806f30d589e53d01ca6e885
Tatu Alatalo <n4927@student.jamk.fi>
2019-09-29 14:03:28 +0300

    Added Harj3 project

---
+++ Code/Harj03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Harj03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
