c5ef9f1af43efa7288998b5aaf379dee0eb2d8a5
Tatu Alatalo <n4927@student.jamk.fi>
2019-10-13 15:43:03 +0300

    added Demo06

---
+++ Code/Demo06/Helper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Demo06
+{
+    class Helper
+    {
+        public static void PrintInfo()
+        {
+            Console.WriteLine("Info");
+        }
+
+        public static int PrintAndReturnNumber()
+        {
+            Console.WriteLine("Info - returning 123");
+            return 123;
+        }
+
+        public static int Sum(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public static int AskNumber(string title)
+        {
+            int number;
+            Console.WriteLine(title);
+            string line = Console.ReadLine();
+            int.TryParse(line, out number);
+            return number;
+        }
+    }
+}
---
+++ Code/Demo06/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Demo06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo06 - classes and methods");
+
+            Helper.PrintInfo();
+
+            // call a method with a return value
+            int number = Helper.PrintAndReturnNumber();
+            Console.WriteLine("number is " + number);
+
+            // call method with parameters
+            int theSum = Helper.Sum(number, 11);
+            Console.WriteLine(theSum);
+
+
+
+            // task, read two numbers from the user
+            /*
+            int number1;
+            int number2;
+
+            Console.WriteLine("Enter first number: ");
+            string line = Console.ReadLine();
+            int.TryParse(line, out number1);
+
+            Console.WriteLine("Enter first number: ");
+            line = Console.ReadLine();
+            int.TryParse(line, out number2);
+
+            theSum = Helper.Sum(number1, number2);
+            Console.WriteLine("Sum is " + theSum);
+            */
+
+            int number1 = Helper.AskNumber("Enter first number ");
+            int number2 = Helper.AskNumber("Enter second number ");
+            Console.WriteLine("Sum is " + Helper.Sum(number1, number2));
+
+
+        }
+    }
+}
