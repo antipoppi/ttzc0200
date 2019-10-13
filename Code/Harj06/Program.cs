9f9b39b25ee64a6f59f9c447caee53db2bae2e07
Tatu Alatalo <n4927@student.jamk.fi>
2019-10-13 17:53:11 +0300

    added harj06

---
+++ Code/Harj06/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Harj06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Utils.Info(); //L06T01
+
+            int theSubtract = Utils.Subtract(50, 13); //L06T02
+            Console.WriteLine(theSubtract);
+
+            double theAverage = Utils.Average(3, 4, 6); //L06T03
+            Console.WriteLine(theAverage);
+
+            double autonKulutus = Utils.AskValue("Paljonko on auton kulutus litraa/100km? "); //L06T04
+            double polttoaineHinta = Utils.AskValue("Paljonko on polttoaineen hinta? ");
+            double kuljettuMatka = Utils.AskValue("Paljonko on kuljettu matka kilometreissä? ");
+            Utils.CalcConsumption(autonKulutus, polttoaineHinta, kuljettuMatka);
+        }
+    }
+}
---
+++ Code/Harj06/Utils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Harj06
+{
+    class Utils
+    {
+        public static void Info() //L06T01
+        {
+            Console.WriteLine("Utils.Info");
+        }
+
+        public static int Subtract(int number1, int number2) //L06T02
+        {
+            return (number1 - number2);
+        }
+
+        public static double Average(double number1, double number2, double number3) //L06T03
+        {
+            return ((number1 + number2 + number3) / 3);
+        }
+
+        public static double AskValue(string title) //L06T04
+        {
+            double number;
+            Console.WriteLine(title);
+            string line = Console.ReadLine();
+            double.TryParse(line, out number);
+            return number;
+        }
+
+        public static void CalcConsumption(double autonKulutus, double polttoaineHinta, double kuljettuMatka)
+        {
+            Console.WriteLine("Matkalla kulunut " + ((autonKulutus * kuljettuMatka) / 100) + " litraa polttoainetta ja polttoaineeseen kului " + (((autonKulutus * kuljettuMatka) / 100) * polttoaineHinta) + " euroa");
+        }
+    }
+}
