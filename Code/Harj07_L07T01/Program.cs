ca1c40645fbb5212c1cd5b55a334f049350d256f
Tatu Alatalo <n4927@student.jamk.fi>
2019-10-21 18:50:40 +0300

    added Harj07_L07T01

---
+++ Code/Harj07_L07T01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Harj07_L07T01
+{
+    class Program
+    {
+        public static int AskNumber(string title)
+        {
+            int number;
+            Console.WriteLine(title);
+            string line = Console.ReadLine();
+            int.TryParse(line, out number);
+            return number;
+        }
+        static void Main(string[] args)
+        {
+            int number = AskNumber("Mikä on ikäsi?");
+
+            if (number < 13)
+            {
+                Console.WriteLine("child");
+            }
+            else if (number >= 13 && number <= 19)
+            {
+                Console.WriteLine("teen");
+            }
+            else if (number >= 20 && number <= 65)
+            {
+                Console.WriteLine("adult");
+            }
+            else
+            {
+                Console.WriteLine("senior");
+            }
+        }
+    }
+}
