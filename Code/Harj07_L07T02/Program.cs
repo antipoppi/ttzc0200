26133780163b2b1f6dd386677e57dce7da7d1076
Tatu Alatalo <n4927@student.jamk.fi>
2019-10-21 19:12:29 +0300

    added Harj07_L07T02

---
+++ Code/Harj07_L07T02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Harj07_L07T02 //Ehto- ja toistorakenteet
+{
+    class Program
+    {
+        public static int AskNumber(string title)
+        {
+            int number;
+            Console.WriteLine(title);
+            string line = Console.ReadLine();
+            int.TryParse(line, out number);
+            return number;
+        }
+        static void Main(string[] args)
+        {
+            int number = AskNumber("Syötä ensimmäinen luku:");
+            int number2 = AskNumber("Syötä toinen luku:");
+            int number3 = AskNumber("Syötä kolmas luku:");
+            if (number > number2 && number > number3)
+            {
+                Console.WriteLine("Suurin syöttämäsi luku on " + number);
+            }
+            else if (number2 > number && number2 > number3)
+            {
+                Console.WriteLine("Suurin syöttämäsi luku on " + number2);
+            }
+            else if (number3 > number && number3 > number2)
+            {
+                Console.WriteLine("Suurin syöttämäsi luku on " + number3);
+            }
+        }
+    }
+}
