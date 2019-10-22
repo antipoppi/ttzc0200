00957c518aa0e2775efc4dfaffc25a971c6e67f5
Tatu Alatalo <n4927@student.jamk.fi>
2019-10-22 18:31:17 +0300

    added Demo09

---
+++ Code/Demo09/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+struct Movie
+{
+    public override string ToString()
+    {
+        return Title + ", " + Director + ", " + Year;
+    }
+    public string Title;
+    public string Director;
+    public int Year;
+
+    // ---
+}
---
+++ Code/Demo09/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Demo09
+{
+    enum Appstatus
+    {
+        Loading,
+        Running,
+        Suspended,
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo09 - enums and structs");
+
+            Appstatus status = Appstatus.Running;
+            switch (status)
+            {
+                case Appstatus.Loading:
+                    Console.WriteLine("App status is Loading");
+                    break;
+                case Appstatus.Running:
+                    Console.WriteLine("App status is Running");
+                    break;
+                case Appstatus.Suspended:
+                    Console.WriteLine("App status is Suspended");
+                    break;
+            }
+
+            Console.WriteLine("App status is " + status);
+
+
+            Movie classic;
+            classic.Title = "Enter the Dragon";
+            classic.Director = "Robert Clouse";
+            classic.Year = 1973;
+
+            Console.WriteLine(classic);
+        }
+    }
+}
