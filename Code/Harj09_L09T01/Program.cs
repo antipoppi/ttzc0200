62501b9097141afdd594b79d1883b63a045e7816
Tatu Alatalo <n4927@student.jamk.fi>
2019-10-22 18:57:02 +0300

    added Harj09_L09T01

---
+++ Code/Harj09_L09T01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Harj09_L09T01 //Enumeraatiot ja tietueet
+{
+    enum Viikonpäivä
+    {
+        Maanantai,
+        Tiistai,
+        Keskiviikko,
+        Torstai,
+        Perjantai,
+        Lauantai,
+        Sunnuntai,
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Viikonpäivä status = Viikonpäivä.Tiistai;
+            switch (status)
+            {
+                case Viikonpäivä.Maanantai:
+                    Console.WriteLine("Tänään on maanantai.");
+                    break;
+                case Viikonpäivä.Tiistai:
+                    Console.WriteLine("Tänään on tiistai.");
+                    break;
+                case Viikonpäivä.Keskiviikko:
+                    Console.WriteLine("Tänään on keskiviikko.");
+                    break;
+                case Viikonpäivä.Torstai:
+                    Console.WriteLine("Tänään on torstai.");
+                    break;
+                case Viikonpäivä.Perjantai:
+                    Console.WriteLine("Tänään on perjantai.");
+                    break;
+                case Viikonpäivä.Lauantai:
+                    Console.WriteLine("Tänään on lauantai.");
+                    break;
+                case Viikonpäivä.Sunnuntai:
+                    Console.WriteLine("Tänään on sunnuntai.");
+                    break;
+            }
+        }
+    }
+}
