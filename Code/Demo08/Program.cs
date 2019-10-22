e9e64ac220dfb2ff315d11c2c60877f473f6948e
Tatu Alatalo <n4927@student.jamk.fi>
2019-10-22 18:03:47 +0300

    added Demo08

---
+++ Code/Demo08/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Demo08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo08 - Hello Debugger!");
+
+            Random rand = new Random();
+
+            int number1 = 10;
+            int number2 = rand.Next(101);
+
+            int temp = number1;
+            number1 = number2;
+            number2 = temp;
+
+            Step();
+
+            StepWithParams(number1, number2);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        public static void Step()
+        {
+            Console.WriteLine("Inside Step");
+        }
+
+        public static void StepWithParams(int param1, int param2)
+        {
+            if (param1 > param2)
+            {
+                Console.WriteLine("param1 is larger than param2");
+            }
+            else
+            {
+                Console.WriteLine("param1 is smaller or equal to param2");
+            }
+
+            param1 += param2;
+            param2 += param2;
+        }
+    }
+}
