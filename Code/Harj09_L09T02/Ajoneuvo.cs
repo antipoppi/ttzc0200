013485154f34469fb04e3857b18c4a2fa9910dee
Tatu Alatalo <n4927@student.jamk.fi>
2019-10-22 19:21:44 +0300

    added Harj09_L09T02

---
+++ Code/Harj09_L09T02/Ajoneuvo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+struct Ajoneuvo
+{
+    public override string ToString()
+    {
+        return Valmistaja + " " + Merkki + ", vm." + Vuosimalli + ", " + Hinta + " euroa";
+    }
+    public string Valmistaja;
+    public string Merkki;
+    public int Vuosimalli;
+    public int Hinta;
+}
---
+++ Code/Harj09_L09T02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Harj09_L09T02 //Enumeraatiot ja tietueet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ajoneuvo auto1;
+            auto1.Valmistaja = "Seat";
+            auto1.Merkki = "Leon";
+            auto1.Vuosimalli = 2005;
+            auto1.Hinta = 3000;
+
+            Console.WriteLine(auto1);
+
+            Ajoneuvo auto2;
+            auto2.Valmistaja = "Volkswagen";
+            auto2.Merkki = "Golf";
+            auto2.Vuosimalli = 1989;
+            auto2.Hinta = 700;
+
+            Console.WriteLine(auto2);
+
+            Ajoneuvo auto3;
+            auto3.Valmistaja = "Volvo";
+            auto3.Merkki = "V60";
+            auto3.Vuosimalli = 2018;
+            auto3.Hinta = 36900;
+
+            Console.WriteLine(auto3);
+        }
+    }
+}
