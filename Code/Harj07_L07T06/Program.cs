c92dd389241f0222e2141a7f4c4aa93a0ce08515
Tatu Alatalo <n4927@student.jamk.fi>
2019-10-22 17:30:45 +0300

    added Harj07_L07T06

---
+++ Code/Harj07_L07T06/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Harj07_L07T06 //Ehto- ja toistorakenteet
+{
+    class Program
+    {
+        public static int AskNumber(string title)
+        {
+            int vuosiluku;
+            Console.WriteLine(title);
+            string line = Console.ReadLine();
+            int.TryParse(line, out vuosiluku);
+            return vuosiluku;
+        }
+        static void Main(string[] args)
+        {
+            int vuosiluku = AskNumber("Syötä vuosiluku tietääksesi, onko kyseinen vuosi karkausvuosi: ");
+            if (vuosiluku % 400 == 0)
+            {
+                Console.WriteLine("On karkausvuosi");
+            }
+            else if (vuosiluku % 100 == 0 || vuosiluku % 4 > 0)
+            {
+                Console.WriteLine("Ei ole karkausvuosi");
+            }
+            else if (vuosiluku % 4 == 0)
+            {
+                Console.WriteLine("On karkausvuosi");
+            }
+        }
+    }
+}
