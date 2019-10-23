ea26be7a007a5d182c181370698ef2c24509058f
Tatu Alatalo <n4927@student.jamk.fi>
2019-10-23 16:32:08 +0300

    added Harj10_L10T01

---
+++ Code/Harj10_L10T01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Harj10_L10T01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle ajoneuvo;
+            CheckEfficiency(out ajoneuvo);
+            Console.WriteLine(ajoneuvo);       
+        }
+        public static void CheckEfficiency(out Vehicle ajoneuvo)
+        {
+            ajoneuvo.Name = "Seat Leon";
+            ajoneuvo.ModelYear = 2005;
+            if (ajoneuvo.ModelYear < 1990)
+            {
+                ajoneuvo.Efficiency = 10;
+            }
+            else if (ajoneuvo.ModelYear >= 1990 && ajoneuvo.ModelYear <= 2010)
+            {
+                ajoneuvo.Efficiency = 50;
+            }
+            else
+            {
+                ajoneuvo.Efficiency = 100;
+            }
+        }
+    }
+}
---
+++ Code/Harj10_L10T01/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+struct Vehicle
+{
+    public string Name;
+    public int ModelYear;
+    public int Efficiency;
+    public override string ToString()
+    {
+        return "Name: " + Name + ". Model year: " + ModelYear + ". Efficiency: " + Efficiency + ".";
+    }
+}
