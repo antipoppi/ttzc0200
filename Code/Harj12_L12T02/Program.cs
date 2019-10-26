ac90cfca86b0fe3d5c5f671104740ff94c0c58b4
Tatu Alatalo <n4927@student.jamk.fi>
2019-10-26 18:37:29 +0300

    added Harj12_L12T02

---
+++ Code/Harj12_L12T02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Harj12_L12T02 //Muistinhallinta, taulukot
+{
+    class Program
+    {
+        public static int AskNumber(string title)
+        {
+            int number;
+            Console.WriteLine(title);
+            string line = Console.ReadLine();
+            int.TryParse(line, out number);
+            return number;
+        }
+        static void Main(string[] args)
+        {
+            int indeksiLkm = AskNumber("Syötä kuinka monta kokonaislukua luodaan: ");
+            int[] taulukko = new int[indeksiLkm];
+            for (int i = 0; i < taulukko.Length; i++)
+            {
+                taulukko[i] = i + 10;
+            }
+            Array.Reverse(taulukko);
+            PrintArray(taulukko);
+
+        }
+        static void PrintArray (int[] taulukko)
+        {
+            foreach (int value in taulukko)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
