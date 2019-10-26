867a358690eae3a015892d34c4bc10d14fc7f449
Tatu Alatalo <n4927@student.jamk.fi>
2019-10-26 19:09:03 +0300

    added Har12_L12T03

---
+++ Code/Harj12_L12T03/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Harj12_L12T03 //Muistinhallinta, taulukot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] taulukko = { 1, 2, 33, 44, 55, 68, 77, 96, 100 };
+            for (int i = 0; i < taulukko.Length; i++)
+            if (taulukko[i] % 2 == 0)
+            {
+                Console.WriteLine(taulukko[i] + " EVEN");
+            }
+            else Console.WriteLine(taulukko[i] + " ");
+        }
+    }
+}
