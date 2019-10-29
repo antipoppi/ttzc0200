0012416e361d39ec82cb26aed7f6581fc6577bcd
Tatu Alatalo <n4927@student.jamk.fi>
2019-10-29 12:03:10 +0200

    updated Harj13_L13T01 and Harj13_L13T02 into v2.0.

--- Code/Harj13_L13T01/Program.cs
+++ Code/Harj13_L13T01/Program.cs
@@ -1,17 +1,49 @@
 ﻿/*  Tehtävänanto:
  *  Toteuta ohjelma joka tallentaa kaikki korttipelin kortit (hertta, ruutu, risti ja pata. Numerot 1-52.) valitsemaasi tietorakenteeseen
-    Tulosta korttipakka konsoliin.
-*/
+ *  Tulosta korttipakka konsoliin.
+ *  
+ *  V2.0: Tehtävä on tehty kokonaan uusiksi. 
+ *  Aikaisemman version lähdekoodi on laitettu kommentiksi ohjelman alle. 
+ *  Korttipakan koko on nyt 52, eikä 56.
+ *  Alunperin käytin tietorakenteena sanakirjaa, mutta se oli tässä tapauksessa kömpelö valinta,
+ *  sillä kortteihin sidottiin myös kokonaisluku arvoksi joka jäi käyttämättä. 
+ *  Lista toimii huomattavasti paremmin tässä tarkoituksessa.
+ */
 
 using System;
 using System.Collections.Generic;
 
 namespace Harj13_L13T01 //Kokoelmat
 {
-    class Program
+    class Program 
     {
         static void Main(string[] args)
         {
+            List<string> korttipakka = new List<string>();
+            for (int i = 1; i < 14; i++)
+            {
+                korttipakka.Add("Hertta " + i.ToString());
+            }
+            for (int i = 1; i < 14; i++)
+            {
+                korttipakka.Add("Ruutu " + i.ToString());
+            }
+            for (int i = 1; i < 14; i++)
+            {
+                korttipakka.Add("Risti " + i.ToString());
+            }
+            for (int i = 1; i < 14; i++)
+            {
+                korttipakka.Add("Pata " + i.ToString());
+            }
+            foreach (string value in korttipakka)
+            {
+                Console.WriteLine(value);
+            }
+
+            /*V1.0 ENSIMMÄINEN VERSIO
+             *
+             * 
             Dictionary<string, int> korttipakka = new Dictionary<string, int>();
             for (int i = 1; i < 15; i++) //loopataan Hertta-korttien luominen aloittamalla indeksistä 1
             {
@@ -33,6 +65,9 @@ namespace Harj13_L13T01 //Kokoelmat
             {
                 Console.WriteLine(kvp.Key);
             }
+             *
+             * 
+             */
         }
     }
 }
--- Code/Harj13_L13T02/Program.cs
+++ Code/Harj13_L13T02/Program.cs
@@ -2,18 +2,65 @@
  *  Jatkoa edelliseen; Toteuta korttipakan sekoittaminen.
  *  Toteuta Shuffle metodi joka sekoittaa pakan kortit satunnaiseen järjestykseen.
  *  Kutsu Suffle metodia ja tulosta korttipakka konsoliin.
-*/
+ *  
+ *  V2.0: Tehtävä on tehty kokonaan uusiksi. 
+ *  Aikaisemman version lähdekoodi on laitettu kommentiksi ohjelman alle. 
+ *  Korttipakan koko on nyt 52, eikä 56.
+ *  Alunperin käytin sanakirjaa, mutta se oli tässä tapauksessa tietorakenteena kömpelö valinta,
+ *  sillä kortteihin sidottiin myös kokonaisluku arvoksi joka jäi käyttämättä. 
+ *  Lista toimii huomattavasti paremmin tässä tarkoituksessa. 
+ *  Lisäksi tässä versiossa on luotu/kutsuttu Shuffle metodi, joka jäi luomatta edellisessä versiossa.
+ *  Summa summarum, opinpahan miten saa sekoitettua sanakirjan halutessa...
+ */
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
+//using System.Linq; V1.0 tarvitsee tämän käyttämistä.
 
 namespace Harj13_L13T01 //Kokoelmat
 {
     class Program
     {
+        public static List<T>Shuffle<T>(List<T> korttipakka)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < korttipakka.Count; i++) //loopataan sekoitus koko pakkaan kortti kerrallaan
+            {
+                int k = rnd.Next(0, i); // antaa kortille satunnaisen arvon jo sekoitettujen korttien välillä.
+                T value = korttipakka[k]; //type parametriin tallennetaan satunnaisen arvotun numeron arvo
+                korttipakka[k] = korttipakka[i]; //tallennetaan k-arvoksi järjestyksessä olevan kortin numero
+                korttipakka[i] = value; //lopulta sekoitettu kortti saa uuden arvon ja sijoitetaan arvotulle kohdalle, toistetaan niin kauan että koko pakka läpi.
+            }
+            return korttipakka;
+        }
         static void Main(string[] args)
         {
+            List<string> korttipakka = new List<string>();
+            for (int i = 1; i < 14; i++)
+            {
+                korttipakka.Add("Hertta " + i.ToString());
+            }
+            for (int i = 1; i < 14; i++)
+            {
+                korttipakka.Add("Ruutu " + i.ToString());
+            }
+            for (int i = 1; i < 14; i++)
+            {
+                korttipakka.Add("Risti " + i.ToString());
+            }
+            for (int i = 1; i < 14; i++)
+            {
+                korttipakka.Add("Pata " + i.ToString());
+            }
+            Shuffle(korttipakka); //kutsutaan Shuffle-metodi
+            foreach (string value in korttipakka)
+            {
+                Console.WriteLine(value);
+            }
+            
+            /*V1.0 ENSIMMÄINEN VERSIO
+             *
+             * 
             Dictionary<string, int> korttipakka = new Dictionary<string, int>();
             for (int i = 1; i < 15; i++)
             {
@@ -45,6 +92,9 @@ namespace Harj13_L13T01 //Kokoelmat
             {
                 Console.WriteLine(kvp.Key);
             }
+             *
+             * 
+             */
         }
     }
 }
