08b006e2092e18c37532dcaf2184c5ee6418e26a
Tatu Alatalo <n4927@student.jamk.fi>
2019-12-04 15:09:28 +0200

    added Harj14_L14T01

---
+++ Code/Harj14_L14T01/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Tehtävänanto:
+ * 
+ * Tee ohjelma joka aiheuttaa System.OverflowException poikkeuksen.
+ * Korjaa ohjelma niin ettei se kaadu.
+ * 
+ */
+
+using System;
+
+namespace Harj14_L14T01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string lukuMerkkeinä = "123456789101112131415";
+                int käännös = int.Parse(lukuMerkkeinä);
+                Console.WriteLine(käännös);
+            } catch (OverflowException ex)
+            {
+                Console.WriteLine("Käännettävä luku on liian suuri! " + ex);
+            }
+        }
+    }
+}
