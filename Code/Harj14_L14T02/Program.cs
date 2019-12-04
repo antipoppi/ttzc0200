531bb2df381572cd497a714eb063111249b73c78
Tatu Alatalo <n4927@student.jamk.fi>
2019-12-04 21:18:07 +0200

    added Harj14_L14T02 and Harj14_L14T03

---
+++ Code/Harj14_L14T02/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Tehtävänanto:
+ * 
+ * Toteuta uusi metodi ja laita metodi heittämään NotImplementedException.
+ * Kokeile kutsua metodia.
+ * 
+ */
+
+using System;
+
+namespace Harj14_L14T02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            joskusJokuPäivä();
+        }
+        public static void joskusJokuPäivä()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ Code/Harj14_L14T03/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Tehtävänanto:
+ * 
+ * Tee taulukko jossa on 5 merkkijonoa.
+ * Kysy käyttäjältä indeksi mihin kohtaan taulukkoa käyttäjä haluaa syöttää uuden tekstin.
+ * Kysy käyttäjältä uusi teksti ja laita se taulukkoon käyttäjän antamaan indeksiin.
+ * Tulosta taulukon sisältö.
+ * Korjaa ohjelma niin ettei se kaadu jos käyttäjä syöttää indeksin joka on taulukon ulkopuolella.
+ * Kerro käyttäjälle mikäli indeksi ei ole kelvollinen ja pyydä syöttämään se uudestaan.
+ */
+
+using System;
+
+namespace Harj14_L14T03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] taulukko = new string[5]; // luodaan tyhjä viiden merkkijonon mittainen taulukko
+            for (int i = 0; i < taulukko.Length; i++)
+            {
+                taulukko[i] = "tyhjä";
+            }
+            Console.Write("Syötä indeksinumero (0-4) mihin kohtaan taulukkoa haluat syöttää tekstin: ");
+            int kysyttyIndeksi = Int32.Parse(Console.ReadLine()); // muuttaa stringin integeriksi
+            Console.Write("Syötä uusi teksti joka sijoitetaan antamaasi indeksinumeroon " + kysyttyIndeksi + ": ");
+            string syötettyTeksti = Console.ReadLine();
+            try
+            {
+                taulukko[kysyttyIndeksi] = syötettyTeksti;
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                do // looppaa pyyntöä validista indeksistä niin kauan, kuin ehdot täyttyvät.
+                {
+                    Console.Write(e.Message + " Syötit kelvottoman indeksinumeron! Syötä uusi väliltä 0-4: ");
+                    kysyttyIndeksi = Int32.Parse(Console.ReadLine());
+                }while (kysyttyIndeksi >= taulukko.Length);
+                taulukko[kysyttyIndeksi] = syötettyTeksti;
+            }
+            foreach (string value in taulukko) // tulostaa taulukon
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
