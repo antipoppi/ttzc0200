7dcdeaa647e47bff2d483d36a30aa2a1a0d060cc
Tatu Alatalo <n4927@student.jamk.fi>
2019-12-08 21:46:06 +0200

    added Harj15_L15T05

---
+++ Code/Harj15_L15T05/Program.cs
@@ -0,0 +1,82 @@
+﻿/* Tehtävänanto:
+ *
+ * Tee ohjelma joka käsittelee TV-ohjelmia.
+ * TV-ohjelman tietoina tulee käsitellä:
+ * 
+ * - ohjelman nimi
+ * - kanava (jolta ohjelma tulee)
+ * - alku- ja loppuaika
+ * - infoteksti ohjelmasta.
+ * 
+ * Luo pääohjelmassa muutamia TV-ohjelmamuuttujia (tiedot voit alustaa suoraan koodissa, ei tarvitse kysyä käyttäjältä) ja tallenna ne levylle.
+ * Mieti käytätkö jotain kokoelmaa apunasi.
+ * Toteuta ohjelmaan myös tiedoston lukeminen ja tulosta TV-ohjelmien tiedot konsoliin.
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Harj15_L15T05 // Tiedostot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<TV> ohjelmat = new List<TV>();
+                ohjelmat.Add(new TV("Täydelliset naiset", "MTV3", "21:00", "22:00", "Tässä jaksossa tapahtuu sitä ja tätä."));
+                ohjelmat.Add(new TV("Selviytyjät Suomi", "4", "19:30", "20:50", "Tällä kertaa viidakon valtijuudesta kisaavat sekä julkkikset että tavikset - tutut ja tuntemattomat!"));
+                ohjelmat.Add(new TV("Top Gear", "Sub", "22:45", "23:55", "Huonoille liikemiehille sopiva sporttisedan, Vauxhall VXR8 Bathurst Edition sekä HSV Ute Maloo, vauhdinnälkäisen duunarin valinta."));
+                ohjelmat.Add(new TV("Arto Nyberg", "TV1", "16:00", "16:45", "Suorassa lähetyksessä haastateltavina taiteilija Arja Saijonmaa."));
+                ohjelmat.Add(new TV("Emmerdale", "MTV3", "10:00", "10:30", "Suru puserossa, osa 1/2."));
+                ohjelmat.Add(new TV("Huutokaupan metsästäjät", "FOX", "16:00", "16:30", "Kausi 11. Jakso 14. Tour de Van Nuys. Ivy etsiskelee polkupyöriä. Mary saa itselleen niin mageen varaston ja Dave joutuu venytyspenkkiin."));
+                TallennaOhjelmat(ohjelmat, "TV-Ohjelmat.txt");
+                Console.WriteLine("Tallennettiin " + ohjelmat.Count + " tv-ohjelman tiedot tekstitiedostoon.");
+                LueOhjelmat(ohjelmat, "TV-Ohjelmat.txt");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public static void TallennaOhjelmat(List<TV> ohjelmat, string tiedostonimi)
+        {
+            StreamWriter tvTiedot = null;
+            try
+            {
+                tvTiedot = new StreamWriter(tiedostonimi);
+                foreach (TV value in ohjelmat)
+                {
+                    tvTiedot.WriteLine(value);
+                }
+                tvTiedot.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ei voi tallentaa ohjelmatietoja: " + ex.Message);
+            }
+            finally
+            {
+                if (tvTiedot != null)
+                {
+                    tvTiedot.Close();
+                }
+            }
+        }
+        public static void LueOhjelmat(List<TV> ohjelmat, string tiedostonimi)
+        {
+            try
+            {
+                string sisältö = File.ReadAllText(tiedostonimi); // tallennetaan tekstitiedoston sisältö stringiin
+                Console.WriteLine("\n" + tiedostonimi + " -tiedoston sisältö on seuraava" + ":\n" + sisältö);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Tiedostoa ei löydy: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Code/Harj15_L15T05/TV.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+[Serializable]
+struct TV
+{
+    public TV (string ohjelmaNimi, string kanava, string aloitus, string lopetus, string info)
+    {
+        Ohjelmanimi = ohjelmaNimi;
+        Kanava = kanava;
+        Aloitusaika = aloitus;
+        Lopetusaika = lopetus;
+        Ohjelmainfo = info;
+    }
+    public override string ToString()
+    {
+        return "- " +  Ohjelmanimi + ", kanavalla " + Kanava + " alkaa kello " + Aloitusaika + " ja päättyy kello " + Lopetusaika + ". " + Ohjelmainfo;
+    }
+    public string Ohjelmanimi;
+    public string Kanava;
+    public string Aloitusaika;
+    public string Lopetusaika;
+    public string Ohjelmainfo;
+}
